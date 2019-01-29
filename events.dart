e5d05d8e6717d65856ca7fab8ba8030fc28e8097
Jake Pucan <ljfreelancer88@users.noreply.github.com>
2019-01-29 21:54:22 +0800

    Update events.dart

--- events.dart
+++ events.dart
@@ -1,11 +1,15 @@
 // onPressed
 
-// onTap
+/* 
+ * onTap
+ * Use onTap to element that naturally unclickable like Container, Text
+ */
+// Block format
 onTap: () { 
   Navigator.push(
 	  context,
 		MaterialPageRoute(builder: (context) => Page('num')),
 	);
 },
-          
+// Inline format. Just replace the curly braces with =>
 onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => Page('science')))
