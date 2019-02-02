c130c788079ffb214b7a9f1670d236d4c18833dc
Jake Pucan <ljfreelancer88@users.noreply.github.com>
2019-02-02 09:55:00 +0800

    Create BoxDecoration.dart

---
+++ BoxDecoration.dart
@@ -0,0 +1,21 @@
+Container(
+  decoration: BoxDecoration(
+    color: const Color(0xff7c94b6),
+    image: DecorationImage(
+      image: ExactAssetImage('images/flowers.jpeg'),
+      fit: BoxFit.cover,
+    ),
+    border: Border.all(
+      color: Colors.black,
+      width: 8.0,
+    ),
+  ),
+)
+
+// References
+https://docs.flutter.io/flutter/painting/BoxDecoration-class.html
+https://medium.com/jlouage/flutter-boxdecoration-cheat-sheet-72cedaa1ba20
+https://docs.flutter.io/flutter/painting/BoxDecoration/BoxDecoration.html
+https://flutterbyexample.com/flutter-widgets/
+https://flutterbyexample.com/gradients-background/
+https://medium.com/flutter-community/flutter-layout-cheat-sheet-5363348d037e
