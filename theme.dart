f364ee77e011e93bb71dd442ba6797321d39db17
Jake Pucan <ljfreelancer88@users.noreply.github.com>
2019-02-02 08:44:07 +0800

    Create theme.dart

---
+++ theme.dart
@@ -0,0 +1,25 @@
+ThemeData(
+  primarySwatch: Colors.pink,
+  primaryColor: const Color(0xFFe91e63),
+  accentColor: const Color(0xFFe91e63),
+  canvasColor: const Color(0xFFfafafa),
+  fontFamily: 'Roboto',
+),
+
+// Usage
+return new MaterialApp(
+  title: 'Generated App',
+    theme: new ThemeData(
+    primarySwatch: Colors.pink,
+    primaryColor: const Color(0xFFe91e63),
+    accentColor: const Color(0xFFe91e63),
+    canvasColor: const Color(0xFFfafafa),  
+    fontFamily: 'Roboto',
+  ),
+  home: new MyHomePage(),
+);
+
+// References
+https://docs.flutter.io/flutter/material/ThemeData-class.html
+https://flutterbyexample.com/flutter-theme-class/
+https://flutter.io/docs/cookbook/design/themes
