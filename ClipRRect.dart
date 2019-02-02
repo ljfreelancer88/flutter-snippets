868955af353df8f53b54c8144e63490f9c471563
Jake Pucan <ljfreelancer88@users.noreply.github.com>
2019-02-02 11:17:40 +0800

    Create ClipRRect.dart

---
+++ ClipRRect.dart
@@ -0,0 +1,15 @@
+return ClipRRect(
+  borderRadius: BorderRadius.only(
+    topLeft: Radius.circular(10.0), 
+    bottomLeft: Radius.circular(10.0)
+  ),
+  child: Container(
+    height: 450.0,
+    color: Colors.blue,
+    child: Text("sdfsd")
+  )
+);
+
+// References
+https://docs.flutter.io/flutter/widgets/ClipRRect-class.html
+https://medium.com/flutter-community/clipping-in-flutter-e9eaa6b1721a
