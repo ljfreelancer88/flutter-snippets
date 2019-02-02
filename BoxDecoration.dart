3c1201a2aba1d7a266b05b158ad27aec49e58cc4
Jake Pucan <ljfreelancer88@users.noreply.github.com>
2019-02-02 10:55:42 +0800

    Update BoxDecoration.dart

--- BoxDecoration.dart
+++ BoxDecoration.dart
@@ -1,14 +1,16 @@
 Container(
   decoration: BoxDecoration(
-    color: const Color(0xff7c94b6),
-    image: DecorationImage(
-      image: ExactAssetImage('images/flowers.jpeg'),
-      fit: BoxFit.cover,
-    ),
     border: Border.all(
       color: Colors.black,
       width: 8.0,
     ),
+    borderRadius: BorderRadius.circular(10.0),
+    //borderRadius: BorderRadius.only(topLeft: Radius.circular(10.0), bottomLeft: Radius.circular(10.0)),
+    color: const Color(0xff7c94b6),
+    image: DecorationImage(
+      image: ExactAssetImage('images/flowers.jpeg'),
+      fit: BoxFit.cover,
+    ),    
   ),
 )
 
