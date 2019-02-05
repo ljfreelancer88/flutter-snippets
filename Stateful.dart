dd779f8ca5c4306c889e89b21feb3955888dc4e8
Jake Pucan <ljfreelancer88@users.noreply.github.com>
2019-02-05 21:57:19 +0800

    Update and rename stateful-widget.dart to Stateful.dart

---
+++ Stateful.dart
@@ -0,0 +1,33 @@
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Stateful Widget',
+      theme: ThemeData(        
+        primarySwatch: Colors.blue,		
+      ),
+      home: MyHomePage(title: 'Home'), // To set a value to a class
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {  
+  final String title;
+  
+  // It can MyHomePage({this.title}) or MyHomePage(this.title)
+  MyHomePage({Key key, @required this.title}) : super(key: key);
+  
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Text(widget.title), // To access the variable from parent class.
+    );
+  }
+}
